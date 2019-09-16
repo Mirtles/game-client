2dc42c147f08e841661f995544057a7869640472
Mirtles <mirte.post@yahoo.co.uk>
2019-09-16 15:38:17 +0200

    Connect to stream

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from "react";
 import "./App.css";
 
-function App() {
-  return <div>Hello World</div>;
+import React from "react";
+import { connect } from "react-redux";
+
+import { setGames } from './actions/games'
+
+const url = 'http://localhost:4000'
+
+class App extends React.Component {
+  source = new EventSource(`${url}/stream`);
+
+  componentDidMount() {
+    this.source.onmessage = event => {
+      const { data } = event
+
+      const games = JSON.parse(data)
+      this.props.setGames(games)
+    }
+  }
+
+  render() {
+    return <div>{this.props.games.map(game => <div>{game.name}</div>)}</div>
+  }
 }
 
-export default App;
+const mapDispatchToProps = {
+  setGames
+};
+
+const mapStateToProps = state => {
+  return {
+    games: state.games
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
