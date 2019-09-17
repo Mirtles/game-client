87e3bca7a98ebf31ab89484e3935cddb36c7ed0f
Mirtles <mirte.post@yahoo.co.uk>
2019-09-17 16:48:22 +0200

    Add routing

--- src/components/GamesList/GamesList.js
+++ src/components/GamesList/GamesList.js
@@ -1,12 +1,16 @@
 import React from "react"
+import { Link } from 'react-router-dom'
 
 export default class GamesList extends React.Component {
   render() {
     return (<div>
       <h1>{this.props.games.length > 0 ? "Available rooms:" : "No rooms available"}</h1>
-      {this.props.games.map((game, i) => <div key={i}>
-        {game.name}
-      </div>)}
+      {this.props.games
+        .map((game, i) => <div key={i}>
+          <Link to={`/lobby/${game.id}`} >
+            {game.name}
+          </Link>
+        </div>)}
     </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './App';
 import store from './store'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-  , document.getElementById('root'));
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
