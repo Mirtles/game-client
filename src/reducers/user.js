c9c251bff69c127863e23aa6743c81f61df81b76
Mirtles <mirte.post@yahoo.co.uk>
2019-09-17 16:16:46 +0200

    Send JWT on game post

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,9 +1,9 @@
 import { JWT } from "../actions/user";
 
-export default function(state = [], action = {}) {
+export default function (state = "", action = {}) {
   switch (action.type) {
     case JWT:
-      return [...state, action.payload];
+      return action.payload;
     default:
       return state;
   }
