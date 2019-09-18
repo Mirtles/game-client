26752070d71c46eae2d294b6dbeeb12d12038e84
andreeaCcss <andreeaccss@gmail.com>
2019-09-18 14:11:46 +0200

    add Round

--- src/components/GamePage/GamePage.js
+++ src/components/GamePage/GamePage.js
@@ -3,21 +3,27 @@ import React from "react";
 export default class GamePage extends React.Component {
   render() {
     return (
-      <div className="imageContainer">
-        <div onClick={e => this.props.onClick(e, "scissors")} className="emoji">
-          <span role="img" aria-label="scissors">
-            ✌️
-          </span>
-        </div>
-        <div onClick={e => this.props.onClick(e, "paper")} className="emoji">
-          <span role="img" aria-label="paper">
-            ✋
-          </span>
-        </div>
-        <div onClick={e => this.props.onClick(e, "rock")} className="emoji">
-          <span role="img" aria-label="rock">
-            ✊
-          </span>
+      <div>
+        <p>Round: {this.props.game.round}</p>
+        <div className="imageContainer">
+          <div
+            onClick={e => this.props.onClick(e, "scissors")}
+            className="emoji"
+          >
+            <span role="img" aria-label="scissors">
+              ✌️
+            </span>
+          </div>
+          <div onClick={e => this.props.onClick(e, "paper")} className="emoji">
+            <span role="img" aria-label="paper">
+              ✋
+            </span>
+          </div>
+          <div onClick={e => this.props.onClick(e, "rock")} className="emoji">
+            <span role="img" aria-label="rock">
+              ✊
+            </span>
+          </div>
         </div>
       </div>
     );
