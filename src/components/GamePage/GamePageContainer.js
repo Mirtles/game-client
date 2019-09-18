f95a6a157f0cc87d06ba5ab17bc44d4fa56be1a6
andreeaCcss <andreeaccss@gmail.com>
2019-09-18 16:49:44 +0200

    update gamepage

--- src/components/GamePage/GamePageContainer.js
+++ src/components/GamePage/GamePageContainer.js
@@ -3,13 +3,26 @@ import { connect } from "react-redux";
 import request from "superagent";
 
 import GamePage from "./GamePage";
-import ScorePage from "../ScorePage/ScorePage"
+import ScorePage from "../ScorePage/ScorePage";
 import url from "../../constants";
 
 class GamePageContainer extends React.Component {
   onClick = (e, choice) => {
+    console.log("i am clicked");
     request
       .put(`${url}/choose/${choice}`)
+      .set("Authorization", `Bearer ${this.props.user}`)
+      // .then(res => {
+      //   // console.log(res);
+      //   alert("yay got " + JSON.stringify(res.body.news));
+      // })
+      .catch(console.error);
+  };
+
+  onScorePage = () => {
+    // console.log("i am clicked");
+    request
+      .put(`${url}/round`)
       .set("Authorization", `Bearer ${this.props.user}`)
       .catch(console.error);
   };
@@ -18,20 +31,27 @@ class GamePageContainer extends React.Component {
     const game =
       this.props.games.length > 0
         ? this.props.games.find(
-          game => game.id === parseInt(this.props.match.params.gameId)
-        )
+            game => game.id === parseInt(this.props.match.params.gameId)
+          )
         : null;
-
+    // console.log(game.users.length);
     return (
       <div>
         {!game ? (
           "Loading... "
+        ) : game.users.length === 1 ? (
+          "Waiting for opponent"
+        ) : game.users.find(user => user.isRoundWinner) ? (
+          <ScorePage game={game} onClick={this.onScorePage} />
         ) : (
-            <div>
-              <GamePage onClick={this.onClick} game={game} />
-              <ScorePage game={game} />
-            </div>
-          )}
+          <GamePage onClick={this.onClick} game={game} />
+        )
+
+        // <div>
+        //   <GamePage onClick={this.onClick} game={game} />
+        //   <ScorePage game={game} />
+        // </div>
+        }
       </div>
     );
   }
--- src/components/ScorePage/ScorePage.js
+++ src/components/ScorePage/ScorePage.js
@@ -1,36 +1,45 @@
-import React from 'react'
+import React from "react";
 
 export default class ScorePage extends React.Component {
   render() {
-    const users = this.props.game.users
-    const userOne = users[0]
-    const userTwo = users[1]
-    const winner = users.find(user => user.isRoundWinner)
-    console.log(users)
-    const rock = "✊"
-    const paper = "✋"
-    const scissors = "✌️"
+    const users = this.props.game.users;
+    const userOne = users[0];
+    const userTwo = users[1];
+    const winner = users.find(user => user.isRoundWinner === true);
+    // console.log(this.props.game);
+    const rock = "✊";
+    const paper = "✋";
+    const scissors = "✌️";
 
-    return (<div>
-      <h1>Round winner: {!winner ? "draw" : winner.name}</h1>
+    return (
       <div>
-        <h2>{userOne.name}</h2>
+        <h1>Round winner: {!winner ? "draw" : winner.name}</h1>
         <div>
-          <span aria-label={userOne.current_choice}>
-            {userOne.current_choice === "rock" ? rock : userOne.current_choice === "paper" ? paper : scissors}
-          </span>
+          <h2>{userOne.name}</h2>
+          <div>
+            <span aria-label={userOne.current_choice}>
+              {userOne.current_choice === "rock"
+                ? rock
+                : userOne.current_choice === "paper"
+                ? paper
+                : scissors}
+            </span>
+          </div>
         </div>
-      </div>
-      <div>
-        <h2>{userTwo.name}</h2>
         <div>
-          <span aria-label={userTwo.current_choice}>
-            {userTwo.current_choice === "rock" ? rock : userTwo.current_choice === "paper" ? paper : scissors}
-          </span>
+          <h2>{userTwo.name}</h2>
+          <div>
+            <span aria-label={userTwo.current_choice}>
+              {userTwo.current_choice === "rock"
+                ? rock
+                : userTwo.current_choice === "paper"
+                ? paper
+                : scissors}
+            </span>
+          </div>
         </div>
+        <button onClick={this.props.onClick}>Next Round</button>
       </div>
-
-    </div>
-    )
+    );
   }
 }
