304a731b6f41904a79ebb36108520929b7bf82ae
andreeaCcss <andreeaccss@gmail.com>
2019-09-18 10:55:24 +0200

    request to join endpoint

--- src/components/GamesList/GamesList.js
+++ src/components/GamesList/GamesList.js
@@ -1,17 +1,40 @@
-import React from "react"
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import request from "superagent";
+import url from "../../constants";
+import { connect } from "react-redux";
 
-export default class GamesList extends React.Component {
+class GamesList extends React.Component {
+  onClick = (e, gameId) => {
+    request
+      .put(`${url}/join/${gameId}`)
+      .set("Authorization", `Bearer ${this.props.user}`)
+      .catch(console.error);
+  };
+
   render() {
-    return (<div>
-      <h1>{this.props.games.length > 0 ? "Available rooms:" : "No rooms available"}</h1>
-      {this.props.games
-        .map((game, i) => <div key={i}>
-          <Link to={`/lobby/${game.id}`} >
-            {game.name}
-          </Link>
-        </div>)}
-    </div>
-    )
+    return (
+      <div>
+        <h1>
+          {this.props.games.length > 0
+            ? "Available rooms:"
+            : "No rooms available"}
+        </h1>
+        {this.props.games.map((game, i) => (
+          <div onClick={e => this.onClick(e, game.id)} key={i}>
+            <Link to={`/lobby/${game.id}`}>{game.name}</Link>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    games: state.games,
+    user: state.user
+  };
+};
+
+export default connect(mapStateToProps)(GamesList);
