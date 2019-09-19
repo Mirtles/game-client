6a382c1b074898e52abda5051938b4a1f4877b12
Mirtles <mirte.post@yahoo.co.uk>
2019-09-19 16:31:55 +0200

    Refactor choices

--- src/components/GamePage/GamePage.js
+++ src/components/GamePage/GamePage.js
@@ -4,26 +4,26 @@ export default class GamePage extends React.Component {
   render() {
     return (
       <div>
-        <p>Round: {this.props.game.round}</p>
         <div className="imageContainer">
-          <div
-            onClick={e => this.props.onClick(e, "scissors")}
-            className="emoji"
-          >
-            <span role="img" aria-label="scissors">
-              ✌️
-            </span>
-          </div>
-          <div onClick={e => this.props.onClick(e, "paper")} className="emoji">
-            <span role="img" aria-label="paper">
-              ✋
-            </span>
-          </div>
+
           <div onClick={e => this.props.onClick(e, "rock")} className="emoji">
             <span role="img" aria-label="rock">
               ✊
             </span>
           </div>
+
+          <div onClick={e => this.props.onClick(e, "paper")} className="emoji">
+            <span role="img" aria-label="paper">
+              ✋
+            </span>
+          </div>
+
+          <div onClick={e => this.props.onClick(e, "scissors")} className="emoji">
+            <span role="img" aria-label="scissors">
+              ✌️
+            </span>
+          </div>
+
         </div>
       </div>
     );
