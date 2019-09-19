acd1d6f8d32b051e0c9336eb7abb07af55c0be5d
andreeaCcss <andreeaccss@gmail.com>
2019-09-19 21:02:31 +0200

    fix refresh bug

--- src/components/ScoreBar/ScoreBar.js
+++ src/components/ScoreBar/ScoreBar.js
@@ -2,19 +2,26 @@ import React from "react";
 
 export default class ScoreBar extends React.Component {
   render() {
+    // console.log(this.props.game);
     const { round, name } = this.props.game;
     const users = this.props.game.users;
     const userOne = users.find(user => user.id === this.props.user.id);
+    // console.log(userOne);
+
     const userTwo = users.find(user => user.id !== this.props.user.id);
 
     return (
-      <div className="scoreBar">
-        <div className="round">Round: {round}</div>
-        <div className="room">Room: {name}</div>
-        <div className="score">
-          Score: {userOne.name} {userOne.score} vs {userTwo.name}{" "}
-          {userTwo.score}
-        </div>
+      <div>
+        {userOne ? (
+          <div className="scoreBar">
+            <div className="round">Round: {round}</div>
+            <div className="room">Room: {name}</div>
+            <div className="score">
+              Score: {userOne.name} {userOne.score} vs {userTwo.name}{" "}
+              {userTwo.score}
+            </div>
+          </div>
+        ) : null}
       </div>
     );
   }
--- src/components/ScorePage/ScorePage.js
+++ src/components/ScorePage/ScorePage.js
@@ -18,51 +18,55 @@ export default class ScorePage extends React.Component {
     const userIsVictorious = gameEnd && userIsWinner;
 
     return (
-      <div className="scorePage">
-        {!gameEnd ? null : userIsVictorious ? (
-          <div className="victory">Victory!</div>
-        ) : (
-          <div className="defeat">Crushing defeat!</div>
-        )}
-        {gameEnd ? null : (
-          <div className="roundScore">
-            {!winner
-              ? "It's a draw!"
-              : userIsWinner
-              ? "You win the round!"
-              : `${winner.name} wins the round.`}
-          </div>
-        )}
-        <div className="scoreDivs">
-          <div className="userScore">
-            <h2 className="nameScore">{userOne.name}</h2>
-            <div className="scoreEmojiOne">
-              <span aria-label={userOne.current_choice}>
-                {userOne.current_choice === "rock"
-                  ? rock
-                  : userOne.current_choice === "paper"
-                  ? paper
-                  : scissors}
-              </span>
+      <div>
+        {userOne ? (
+          <div className="scorePage">
+            {!gameEnd ? null : userIsVictorious ? (
+              <div className="victory">Victory!</div>
+            ) : (
+              <div className="defeat">Crushing defeat!</div>
+            )}
+            {gameEnd ? null : (
+              <div className="roundScore">
+                {!winner
+                  ? "It's a draw!"
+                  : userIsWinner
+                  ? "You win the round!"
+                  : `${winner.name} wins the round.`}
+              </div>
+            )}
+            <div className="scoreDivs">
+              <div className="userScore">
+                <h2 className="nameScore">{userOne.name}</h2>
+                <div className="scoreEmojiOne">
+                  <span aria-label={userOne.current_choice}>
+                    {userOne.current_choice === "rock"
+                      ? rock
+                      : userOne.current_choice === "paper"
+                      ? paper
+                      : scissors}
+                  </span>
+                </div>
+              </div>
+              <div className="userScore">
+                <h2 className="nameScore">{userTwo.name}</h2>
+                <div className="scoreEmojiTwo">
+                  <span aria-label={userTwo.current_choice}>
+                    {userTwo.current_choice === "rock"
+                      ? rock
+                      : userTwo.current_choice === "paper"
+                      ? paper
+                      : scissors}
+                  </span>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="userScore">
-            <h2 className="nameScore">{userTwo.name}</h2>
-            <div className="scoreEmojiTwo">
-              <span aria-label={userTwo.current_choice}>
-                {userTwo.current_choice === "rock"
-                  ? rock
-                  : userTwo.current_choice === "paper"
-                  ? paper
-                  : scissors}
-              </span>
-            </div>
-          </div>
-        </div>
 
-        {gameEnd ? null : (
-          <button onClick={this.props.onClick}>Next Round</button>
-        )}
+            {gameEnd ? null : (
+              <button onClick={this.props.onClick}>Next Round</button>
+            )}
+          </div>
+        ) : null}
       </div>
     );
   }
