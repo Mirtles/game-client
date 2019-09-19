00909961e4342281311cd66ee9ba97d7580b0e17
andreeaCcss <andreeaccss@gmail.com>
2019-09-19 13:28:55 +0200

    constants change

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-const url = "http://localhost:4000";
+// const url = "http://localhost:4000";
 
-// const url = "https://powerful-mountain-51142.herokuapp.com";
+const url = "https://powerful-mountain-51142.herokuapp.com";
 
 export default url;
